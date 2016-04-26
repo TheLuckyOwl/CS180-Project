8396d63b39949658db0ac52e442db036fd5e1dd2
dtara003 <dtara003@ucr.edu>
2016-04-25 20:37:06 -0700

    Campus Safety page done
    - descriptions of safety resources
    - auto-fill call buttons that redirect user to outside of app
    - set up to prepare calls to campus safety escort services, UCPD, and
    911
    - expand and add other sources in the near future?

--- app/src/main/java/theunderground/com/ucrmap/CampusSafetyActivity.java
+++ app/src/main/java/theunderground/com/ucrmap/CampusSafetyActivity.java
@@ -1,24 +1,55 @@
 package theunderground.com.ucrmap;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
  * Created by Larry Parsons on 4/14/2016.
  */
 public class CampusSafetyActivity extends Activity{
-    TextView mCallUCPD = null;
-    TextView mCallCSES = null;
-    TextView mCall911 = null;
 
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.campus_safety_activity_layout);
 
-        mCallUCPD = (TextView)findViewById(R.id.callUCPD);
-        mCallCSES = (TextView)findViewById(R.id.callCSES);
-        mCallCSES = (TextView)findViewById(R.id.call911);
+        Button mCSES = (Button) findViewById(R.id.callCSES);
+        Button mUCPD = (Button) findViewById(R.id.callUCPD);
+        Button m911 = (Button) findViewById(R.id.call911);
+
+        mCSES.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_DIAL);
+                i.setData(Uri.parse("tel:9518273772"));
+                startActivity(i);
+            }
+        });
+
+        mUCPD.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_DIAL);
+                i.setData(Uri.parse("tel:9518275222"));
+                startActivity(i);
+            }
+        });
+
+        m911.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_DIAL);
+                i.setData(Uri.parse("tel:911"));
+                startActivity(i);
+            }
+        });
     }
 }
