f2c210872411e52574c21f2ee6f5e9b603cb701e
dtara003 <dtara003@ucr.edu>
2016-05-26 00:24:34 -0700

    Splash Activity Done
    For the sake of having an opening image on our power point, UGH

---
+++ app/src/main/java/theunderground/com/ucrmap/SplashActivity.java
@@ -0,0 +1,33 @@
+package theunderground.com.ucrmap;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Dharti on 5/26/2016.
+ */
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    sleep(3500);
+
+                    Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(i);
+
+                    finish();
+                } catch (Exception e) {
+
+                }
+            }
+        };
+
+        background.start();
+    }
+}
